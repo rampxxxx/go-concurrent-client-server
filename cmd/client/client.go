8e1b589607fade20c020a0e212e239c2bab0d36d
javier <rampxxxx@gmail.com>
2021-09-10 13:04:19 +0200

    Draft. Concurrent client and server.
    Signed-off-by: javier <rampxxxx@gmail.com>

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -4,23 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
+	"strconv"
 )
 
-func main() {
+func snd_message(msg string, sync chan int) {
+
 	conn, error := net.Dial("tcp", "127.0.0.1:8000")
 	if conn != nil {
 		for {
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Print("Introduce texto a enviar:")
-			text, _ := reader.ReadString('\n')
+			//reader := bufio.NewReader(os.Stdin)
+			//fmt.Print("Introduce texto a enviar:")
+			//text, _ := reader.ReadString('\n')
 			// SEND TO SERVER ;-)
-			fmt.Fprintf(conn, text+"\n")
+			fmt.Fprintf(conn, msg+"\n")
 			// Wait reply
 			message, _ := bufio.NewReader(conn).ReadString('\n')
 			fmt.Print("Recibido del servidor:" + message)
+			i := <-sync
+			fmt.Print("Gorutine %v ends:" + strconv.FormatInt(int64(i), 10))
 		}
 	} else {
 		fmt.Print("Error al connectar :" + error.Error())
+	}
+}
+
+func main() {
+	fmt.Print("Starting client\n")
+	synchro := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Client %v \n", strconv.FormatInt(int64(i), 10))
+		synchro <- i
+		go snd_message("Hola"+strconv.FormatInt(int64(i), 10), synchro)
 	}
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,15 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
+
+func rcv_message(conn net.Conn) {
+	message, error := bufio.NewReader(conn).ReadString('\n')
+	if error == nil {
+		fmt.Print("Message received:", string(message))
+	} else {
+		fmt.Printf("Error %v waiting client:", error.Error())
+		os.Exit(1)
+	}
+}
 
 func main() {
 	fmt.Println("Start server...")
 
-	ln, _ := net.Listen("tcp", ":8000")
-	conn, _ := ln.Accept()
+	ln, _ := net.Listen("tcp", "127.0.0.1:8000")
 	for {
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message received:", string(message))
+		conn, _ := ln.Accept()
+		go rcv_message(conn)
 	}
 }
