c2a2b68759110345e514ad7ea0b9beb898008d4d
javier <rampxxxx@gmail.com>
2021-09-11 11:38:06 +0200

    Basic working client/server concurrent example
    Client. Wait for channel done (joining ;-).
    Server. Apply QoS based in a bool channel.
    
    Signed-off-by: javier <rampxxxx@gmail.com>

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -7,33 +7,40 @@ import (
 	"strconv"
 )
 
-func snd_message(msg string, sync chan int) {
+func snd_message(msg string /*sync chan int,*/, i int, done chan bool) {
 
+	fmt.Printf("Connectando %v\n", i)
 	conn, error := net.Dial("tcp", "127.0.0.1:8000")
-	if conn != nil {
-		for {
-			//reader := bufio.NewReader(os.Stdin)
-			//fmt.Print("Introduce texto a enviar:")
-			//text, _ := reader.ReadString('\n')
-			// SEND TO SERVER ;-)
-			fmt.Fprintf(conn, msg+"\n")
-			// Wait reply
-			message, _ := bufio.NewReader(conn).ReadString('\n')
-			fmt.Print("Recibido del servidor:" + message)
-			i := <-sync
-			fmt.Print("Gorutine %v ends:" + strconv.FormatInt(int64(i), 10))
-		}
+	if error == nil {
+		fmt.Printf("Exito Connectando %v!\n", i)
+		defer conn.Close()
+		//reader := bufio.NewReader(os.Stdin)
+		//fmt.Print("Introduce texto a enviar:")
+		//text, _ := reader.ReadString('\n')
+		// SEND TO SERVER ;-)
+		fmt.Fprintf(conn, msg+"\n")
+		// Wait reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Printf("Recibido del servidor (%v): %v\n", i, message)
+		//i := <-sync
+		fmt.Printf("Gorutine %v ends\n", strconv.FormatInt(int64(i), 10))
 	} else {
-		fmt.Print("Error al connectar :" + error.Error())
+		fmt.Printf("Error al connectar (%v)", error.Error())
 	}
+	done <- true
 }
 
 func main() {
+	done := make(chan bool)
 	fmt.Print("Starting client\n")
-	synchro := make(chan int, 10)
+	//synchro := make(chan int, 10)
 	for i := 0; i < 10; i++ {
-		fmt.Printf("Client %v \n", strconv.FormatInt(int64(i), 10))
-		synchro <- i
-		go snd_message("Hola"+strconv.FormatInt(int64(i), 10), synchro)
+		fmt.Printf("Calling gorutine %v \n", strconv.FormatInt(int64(i), 10))
+		//synchro <- i
+		go snd_message("Hola"+strconv.FormatInt(int64(i), 10) /*synchro,*/, i, done)
+	}
+
+	for i := 0; i < 10; i++ {
+		<-done
 	}
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,26 +3,43 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
-func rcv_message(conn net.Conn) {
+func rcv_message(conn net.Conn, syn chan bool) {
+	defer conn.Close()
 	message, error := bufio.NewReader(conn).ReadString('\n')
 	if error == nil {
-		fmt.Print("Message received:", string(message))
+		fmt.Printf("Message received:(%v)\n", string(message))
+		fmt.Fprintf(conn, "Desde el servidor\n"+string(message))
 	} else {
-		fmt.Printf("Error %v waiting client:", error.Error())
-		os.Exit(1)
+		fmt.Printf("Error %v waiting client\n", error.Error())
+		//os.Exit(1)
 	}
+	syn <- true
 }
 
 func main() {
+	synchro := make(chan bool) // one at a time, qos? :-)
 	fmt.Println("Start server...")
 
-	ln, _ := net.Listen("tcp", "127.0.0.1:8000")
+	fmt.Println("Listening ...")
+	listen, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatalf("Error listening ... (%s)", err)
+	}
+	defer listen.Close()
 	for {
-		conn, _ := ln.Accept()
-		go rcv_message(conn)
+		fmt.Println("Locking channel ...")
+		fmt.Println("Accepting connection ...")
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatalf("Error accept ... (%s)", err)
+			continue
+		}
+		fmt.Println("Handling connection ...")
+		go rcv_message(conn, synchro)
+		<-synchro
 	}
 }
